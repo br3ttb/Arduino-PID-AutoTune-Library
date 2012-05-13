29c5dacbbfab7bde191aa830aa4a54858cad1864
t0mpr1c3 <t0mpr1c3@gmail.com>
2012-05-13 13:58:53 +0200

    C port. Method names changed so longer works with example.

--- PID_AutoTune_v0/PID_AutoTune_v0.h
+++ PID_AutoTune_v0/PID_AutoTune_v0.h
@@ -1,63 +1,88 @@
-#ifndef PID_AutoTune_v0
-#define PID_AutoTune_v0
-#define LIBRARY_VERSION	0.0.0
+#ifndef pid_autotune_v0
+#define pid_autotune_v0
+#define pid_autotune_LIBRARY_VERSION	0.0.0
 
-#define PI 0
-#define PID 1
-#define PESSEN 2
-#define SOME_OVERSHOOT 3
-#define NO_OVERSHOOT 4
+#include <avr/io.h>
+#include <PID_v1.h>
 
-class PID_ATune
+enum control
 {
-	public:
-	//commonly used functions **************************************************************************
-		PID_ATune(double*, double*);                       // * Constructor.  links the Autotune to a given PID
-		int Runtime();						   			   	// * Similar to the PID Compute function, returns non 0 when done
-		void Cancel();									   	// * Stops the AutoTune	
-
-		void SetOutputStep(double);						   	// * how far above and below the starting value will the output step?	
-		double GetOutputStep();							   	// 
-
-		void SetControlType(int); 						   	// * Determies if the tuning parameters returned will be PI (D=0)
-		int GetControlType();							   	//   or PID.  (0=PI, 1=PID, 2=Pessen rule, 3="some overshoot" rule, 4="no overshoot" rule)			
-
-		void SetLookbackSec(int);							// * how far back are we looking to identify peaks
-		int GetLookbackSec();								//
-
-		void SetNoiseBand(double);							// * the autotune will ignore signal chatter smaller than this value
-		double GetNoiseBand();								//   this should be accurately set
-
-		void SetDither(double);								// * noise added to input to smooth quantization errors
-		double GetDither();									//   set to smallest step value in input range
-
-		double GetKp();										// * once autotune is complete, these functions contain the
-		double GetKi();										//   computed tuning parameters.  
-		double GetKd();										//
-
-	private:
-		void FinishUp();
-		bool isMax, isMin;
-		double *input, *output;
-		double setpoint;
-		double noiseBand;
-		int controlType;
-		double dither, randomNumber;
-		bool running;
-		unsigned long peak1, peak2, lastTime;
-		int sampleTime;
-		int nLookBack;
-		int peakType;
-		double lastInputs[100];
-		double peaks[10];
-		int peakCount;
-		bool justchanged;
-		bool justevaled;
-		int initCount;
-		double absMax, absMin;
-		double oStep;
-		double outputStart;
-		double Ku, Pu, Kp, Ti, Td;
+	PI = 0,	
+	CLASSIC_PID = 1,
+	PESSEN = 2,
+	SOME_OVERSHOOT = 3,
+	NO_OVERSHOOT = 4
 };
-#endif
 
+enum peak
+{
+	MINIMUM = -1,
+	NON_PEAK = 0,
+	MAXIMUM = 1
+};
+
+typedef struct
+{
+// public
+	double* input;
+	double* output;
+	
+// private
+	double Set_point;
+	enum control Control_type;
+	double Dither;
+	enum boolean Running;
+	enum boolean Is_max;
+	enum boolean Is_min;
+	uint32_t Peak1;
+	uint32_t Peak2;
+	uint32_t Last_time;
+	uint32_t Sample_time;
+	uint8_t Nlookback;
+	enum peak Peak_type;
+	double Last_inputs[100];
+	double Peaks[10];
+	uint8_t Peak_count;
+	enum boolean Just_changed;
+	enum boolean Just_evaled;
+	uint8_t Init_count;
+	double Abs_max;
+	double Abs_min;
+	double Relay_amp;
+	double Relay_hyst;
+	double Output_start;
+	double Ku, Pu, Kp, Ti, Td;
+} atune_t;
+
+// public:
+	
+//commonly used functions **************************************************************************
+	atune_t* atune_new( double*, double* );			// * Constructor.  Links the autotune to a given pid
+	enum boolean atune_runtime( atune_t* );			// * Similar to the pid compute function, returns TRUE when done
+	void atune_cancel( atune_t* );				// * Stops the autotune	
+
+	void atune_set_relay_amp( atune_t*, double );		// * set relay amplitude	
+	double atune_get_relay_amp( atune_t* );			// * get relay amplitude
+
+	void atune_set_control_type( atune_t*, uint8_t );	// * set formula for calculating the tuning parameters
+	enum control atune_get_control_type( atune_t* );	// * get formula for calculating the tuning parameters			
+
+	void atune_set_lookback_sec( atune_t*, uint16_t );	// * set look back distance to identify peaks
+	uint16_t atune_get_lookback_sec( atune_t* );		// * get look back distance to identify peak
+
+	void atune_set_relay_hyst( atune_t*, double );		// * set relay relay_hysteresis
+	double atune_get_relay_hyst( atune_t* );		// * get relay relay_hysteresis
+
+	void atune_set_dither( atune_t*, double );		// * noise added to input to smooth quantization errors
+	double atune_get_dither( atune_t* );			//   set to smallest step value in input range
+
+	double atune_get_Kp( atune_t* );			// * once autotune is complete, these functions contain the
+	double atune_get_Ki( atune_t* );			//   computed tuning parameters.  
+	double atune_get_Kd( atune_t* );			//
+
+/*	
+private:
+	void Atune_finish_up(atune_t*);				// * calculate gain parameters
+*/
+
+#endif
