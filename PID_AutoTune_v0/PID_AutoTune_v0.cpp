2c8f6f27c6fb3474f0cd208d326a82bb0d9854b6
Tom Price <t0mpr1c3@gmail.com>
2013-10-26 20:07:09 -0400

    AMIGOf PI tuning, rationalized peak detection, finite state machine, expanded range of simulations in example sketch

--- PID_AutoTune_v0/PID_AutoTune_v0.cpp
+++ PID_AutoTune_v0/PID_AutoTune_v0.cpp
@@ -10,12 +10,13 @@ PID_ATune::PID_ATune(double* Input, double* Output)
 {
   input = Input;
   output = Output;
+
+  // constructor defaults
   controlType = ZIEGLER_NICHOLS_PI;
   noiseBand = 0.5;
-  running = false;
-  oStep = 30;
+  state = AUTOTUNER_OFF;
+  oStep = 10;
   SetLookbackSec(10);
-  lastTime = millis();
 #ifdef DITHER
   dither = 0.0;
 #endif
@@ -23,26 +24,71 @@ PID_ATune::PID_ATune(double* Input, double* Output)
 
 void PID_ATune::Cancel()
 {
-  running = false;
-} 
+  state = AUTOTUNER_OFF;
+}
+
+// check that recent inputs are equal
+// give or take expected noise
+bool PID_ATune::CheckStable()
+{
+  double max = inputCount[0];
+  double min = inputCount[0];
+  for (byte i = inputCount; i > 0; i--)
+  {
+    if (max < inputCount[i])
+    {
+      max = inputCount[i];
+    }
+    if (min < inputCount[i])
+    {
+      min = inputCount[i];
+    }
+  } 
+#ifdef DITHER
+  return ((max - min) <= noiseBand + 2 * dither)
+#else
+  return ((max - min) <= noiseBand)
+#endif
+}
 
 bool PID_ATune::Runtime()
 {
-  justevaled = false;
-  if ((peakCount > 9) && running)
-  {
-    running = false;
-    FinishUp();
-    return true;
-  }
   unsigned long now = millis();
 
-  if ((now - lastTime) < sampleTime) 
+  if (state == AUTOTUNER_OFF)
+  { 
+    // initialize working variables the first time around
+    peakType = NOT_A_PEAK;
+    inputCount = 0;
+    peakCount = 0;
+    setpoint = refVal;
+    outputStart = *output;
+    originalNoiseBand = noiseBand;
+    
+    // move to new state
+    if (type == AMIGOF_PI)
+    {
+      state = STEADY_STATE_AT_BASELINE;
+    }
+    else
+    {
+      state = RELAY_STEP_UP;
+    }
+  }
+
+  // otherwise check ready for new input
+  else if ((now - lastTime) < sampleTime) 
   {
     return false;
   }
+
+  // get new input
   lastTime = now;
   double refVal = *input;
+
+  // local variable, true if a peak is discovered
+  // that has a different sign to the previous peak
+  bool justChanged = false; 
 
 #ifdef DITHER
   // dither input value to smooth quantization error
@@ -50,50 +96,47 @@ bool PID_ATune::Runtime()
   {
     // add random noise from triangular probability density function 
     // centred on 0 and with range (-dither, dither)
-    refVal += ( random(-dither, dither) + random(-dither, dither) ) / 2.0;
+    refVal += ( random(-dither, dither) + random(-dither, dither) ) * 0.5;
   }
 #endif
 
-  justevaled = true;
-  if (!running)
-  { //initialize working variables the first time around
-    peakType = NON_PEAK;
-    peakCount = 0;
-    justchanged = false;
-    absMax = refVal;
-    absMin = refVal;
-    setpoint = refVal;
-    running = true;
-    outputStart = *output;
+  // check input and change relay state if necessary
+  if (state == RELAY_STEP_UP) && (refVal > setpoint + noiseBand))
+  {
+    state = RELAY_STEP_DOWN;
+  }
+  else if ((state == RELAY_STEP_DOWN) && (refVal < setpoint - noiseBand))
+  {
+    state = RELAY_STEP_UP;
+  }
+
+  // set output
+  if (state & (STEADY_STATE_AFTER_STEP_UP | RELAY_STEP_UP))
+  {
     *output = outputStart + oStep;
   }
-  else
-  {
-    if (refVal > absMax)
-    {
-      absMax = refVal;
-    }
-    if (refVal < absMin)
-    {
-      absMin = refVal;
-    }
-  }
-
-  // oscillate the output base on the input's relation to the setpoint
-
-  if (refVal > setpoint + noiseBand)
+  else if 
+  if (state == RELAY_STEP_DOWN)
   {
     *output = outputStart - oStep;
   }
-  else if (refVal < setpoint - noiseBand)
+
+  // store initial inputs
+  // we don't want to trust the maxes or mins
+  // until the input array is full
+  inputCount++;
+  if (inputCount <= nLookBack)
   {
-    *output = outputStart + oStep;
+    lastInputs[nLookBack - inputCount] = refVal;
+    return false;
   }
-  isMax = true;
-  isMin = true;
 
   // identify peaks
-  for (int i = nLookBack - 1; i >= 0; i--)
+  // and shift lastInputs[]
+  inputCount = nLookBack;
+  bool isMax = true;
+  bool isMin = true;
+  for (byte i = inputCount - 1; i >= 0; i--)
   {
     double val = lastInputs[i];
     if (isMax)
@@ -104,118 +147,244 @@ bool PID_ATune::Runtime()
     {
       isMin = (refVal < val);
     }
-    lastInputs[i+1] = lastInputs[i];
+    lastInputs[i + 1] = lastInputs[i];
   }
-  lastInputs[0] = refVal;  
+  lastInputs[0] = refVal; 
 
-  if (nLookBack < 9)
-  {  
-    // we don't want to trust the maxes or mins until the inputs array has been filled	
-    return false;
+  // for AMIGOf tuning rule, perform an initial
+  // step change to calculate process gain K_process
+  if (state & (STEADY_STATE_AT_BASELINE | STEADY_STATE_AFTER_STEP_UP))
+  {
+    // check that all the recent inputs are 
+    // equal give or take expected noise
+    if CheckStable()
+    {
+      double avgInput = 0.0;
+      for (byte i = 0; i < inputCount; i++)
+      {
+        avgInput += lastInputs[i];
+      }
+      avgInput /= inputCount + 1;
+      if (state == STEADY_STATE_AT_BASELINE)
+      {
+        state == STEADY_STATE_AFTER_STEP_UP;
+        peaks[0] = avgInput;   
+        return false;
+      }
+      // else state == STEADY_STATE_AFTER_STEP_UP
+      // calculate process gain
+      K_process = (avgInput - peaks[0]) / oStep;
+      state == RELAY_STEP_DOWN;
+      return false;
+    }
   }
   
+  // increment peak count 
+  // and record peak time 
+  // for both maxima and minima 
   if (isMax)
   {
-    if (peakType == NON_PEAK)
+    if (peakType == NOT_A_PEAK)
     {
       peakType = MAXIMUM;
     }
     if (peakType == MINIMUM)
     {
       peakType = MAXIMUM;
-      justchanged = true;
-      peak2 = peak1;
+      justChanged = true;
+      peakCount++;
     }
-    peak1 = now;
-    peaks[peakCount] = refVal;
-
   }
   else if (isMin)
   {
-    if (peakType == NON_PEAK)
+    if (peakType == NOT_A_PEAK)
     {
       peakType = MINIMUM;
     }
     if (peakType == MAXIMUM)
     {
       peakType = MINIMUM;
+      justChanged = true;
       peakCount++;
-      justchanged = true;
     }
-
-    if (peakCount < 10)
+  }
+  if (isMax || isMin)
+  {
+    peakTime[0] = now;
+    if (peakCount < 20)
     {
       peaks[peakCount] = refVal;
     }
   }
+  if (justChanged)
+  {
+    peakTime[2] = peakTime[1];
+    peakTime[1] = peakTime[0];
+  }
 
-  if (justchanged && peakCount>2)
+  // check for convergence of induced oscillation
+  // convergence assessed on last 4 peaks (1.5 cycles)
+  // not just on maxima
+  double inducedAmplitude = 0.0;
+  double phaseLag;
+  if (justChanged && (peakCount > 3))
   { 
-    //we've transitioned.  check if we can autotune based on the last peaks
-    double avgSeparation = ( abs( peaks[peakCount - 1] - peaks[peakCount - 2] ) + 
-                             abs( peaks[peakCount - 2] - peaks[peakCount - 3] ) ) / 2;
-    if (avgSeparation < PEAK_AMPLITUDE_TOLERANCE * (absMax - absMin))
+    // convergence assessed based on last 4 peaks (1.5 cycles)
+    byte i;
+    double absMax = peaks[peakCount - 1];
+    double absMin = peaks[peakCount - 1];
+    for (i = peakCount - 1; i > peakCount - 3; i--)
     {
-      FinishUp();
-      running = false;
-      return true;
+      inducedAmplitude += abs(peaks[i] - peaks[i - 1]); 
+      if (absMax < peaks[i - 1])
+      {
+         absMax = peaks[i - 1];
+      }
+      if (absMin > peaks[i - 1])
+      {
+         absMin = peaks[i - 1];
+      }
+    }
+    inducedAmplitude /= 3.0;
+
+    // source for AMIGOf PI auto tuning method:
+    // "Revisiting the Ziegler-Nichols tuning rules for PI control — 
+    //  Part II. The frequency response method."
+    // T. Hägglund and K. J. Åström
+    // Asian Journal of Control, Vol. 6, No. 4, pp. 469-482, December 2004
+    // http://www.ajc.org.tw/pages/paper/6.4PD/AC0604-P469-FR0371.pdf
+    if (type == AMIGOF_PI)
+    {
+      // calculate phase lag
+      // NB hysteresis = 2 * noiseBand;
+      phaseLag = CONST_PI - asin(2.0 * noiseBand / inducedAmplitude); 
+    }
+
+    if (peakCount == 20) 
+    {
+      // 10 cycles is enough already!
+      state = CONVERGED;
+    }
+
+    // check that phase lag is within acceptable bounds
+    else if ((type == AMIGOF_PI) && (abs(phaseLag / CONST_PI * 180.0 - 130.0) > 10.0))
+    {
+      // phase lag outside the desired range between 120° and 140°
+      // set noiseBand to halfway between old value and new estimate
+      double newHysteresis = inducedAmplitude * sin(phaseLag);
+      noiseBand = 0.5 * (newHysteresis * 0.5 + noiseBand);
+      return false;
+    }
+
+    // check convergence criterion for induced oscillation
+    if (abs((absMax - absMin) - inducedAmplitude) / inducedAmplitude < PEAK_AMPLITUDE_TOLERANCE)
+    {
+      state = CONVERGED;
     }
   }
-  justchanged = false;
-  return false;
-}
+  
+  if (state != CONVERGED)
+  {
+    return false;
+  }
 
-void PID_ATune::FinishUp()
-{
+  // autotune algorithm has converged 
+  // finish up by calculating gain parameters
+  // and restting auttuner variables
+
+  state == AUTOTUNER_OFF;
   *output = outputStart;
+  noiseBand = originalNoiseBand;
 
   // generate tuning parameters
-  // using Second Ziegler-Nichols method (closed loop)
+  // using relay method
   
 #ifdef DITHER
   // calculate swing from highest to lowest input
   // net of dither range
-  double induced_amplitude = (absMax - dither) - (absMin + dither);
-#else
-  double induced_amplitude = (absMax - absMin);
+  inducedAmplitude -= 2 * dither;
 #endif
   
   // calculate relay amplitude as twice oStep
-  double relay_amplitude = (2.0 * oStep);
+  double relayAmplitude = (2.0 * oStep);
   
-  // NB peak-to-peak amplitude of relay signal is 2 * p->Relay_amp
-  Ku = 4.0 *  relay_amplitude / (induced_amplitude * 3.14159265358979); // ultimate gain
-  Pu = (double) (peak1 - peak2) / 1000.0; // ultimate period in seconds
+  // calculate ultimate gain
+  double Ku = 4.0 * relayAmplitude / (inducedAmplitude * CONST_PI); 
+
+  // calculate ultimate period in seconds
+  double Pu = (double) (peakTime[0] - peakTime[2]) / 1000.0; 
 
   // calculate gain parameters
   switch(controlType)
   {
+  case AMIGOF_PI:
+    double kappa_phi = Ku / K_process;
+    double a =  2.50 - 0.92 * phaseLag;
+    double b = 10.75 - 4.01 * phaseLag;
+    double c = -3.05 + 1.72 * phaseLag;
+    double d = -6.10 + 3.44 * phaseLag;
+    Kp = a / (1.0 + b * kappa_phi)     * Ku;
+    Ti = c / (1.0 + d * kappa_phi)**2  * Pu;
+    Td = 0.0;
+    break;
   case ZIEGLER_NICHOLS_PID:
     Kp = 0.6   * Ku;
     Ti = 0.5   * Pu;
     Td = 0.125 * Pu;
     break;
-  case PESSEN:
+  // source for Pessen Integral, Some Overshoot,
+  // and No Overshoot rules:
+  // "Rule-Based Autotuning Based on Frequency Domain Identification" 
+  // by Anthony S. McCormack and Keith R. Godfrey
+  // IEEE Transactions on Control Systems Technology, vol 6 no 1, January 1998.
+  // as reported on http://www.mstarlabs.com/control/znrule.html
+  case PESSEN_INTEGRAL_PID:
     Kp = 0.7   * Ku;
     Ti = 0.4   * Pu;
     Td = 0.15  * Pu;
     break;
-  case SOME_OVERSHOOT:
+  case SOME_OVERSHOOT_PID:
     Kp = 0.33  * Ku;
     Ti = 0.5   * Pu;
     Td = 0.33  * Pu;
     break;
-  case NO_OVERSHOOT:
+  case NO_OVERSHOOT_PID:
     Kp = 0.2   * Ku;
     Ti = 0.5   * Pu;
     Td = 0.33  * Pu;
     break;
+  // source of Tyreus-Luyben and Ciancone-Marlin rules:
+  // "Autotuning of PID Controllers: A Relay Feedback Approach",
+  //  by Cheng-Ching Yu, 2nd Edition, p.18
+  case TYREUS_LUYBEN_PI: // for lag dominated processes
+    Kp = Ku / 3.2;
+    Ti = Pu / 0.45;
+    Td = 0.0;  
+    break;
+  case TYREUS_LUYBEN_PID: // for lag dominated processes
+    Kp = Ku / 2.2;
+    Ti = Pu / 0.45;
+    Td = Pu / 6.3;  
+    break;
+  case CIANCONE_MARLIN_PID: // for delay dominated processes
+    Kp = Ku / 3.3;
+    Ti = Pu / 4.4;
+    Td = Pu / 8.1;  
+    break;
+  case CIANCONE_MARLIN_PI: // for delay dominated processes
+    Kp = Ku / 3.3;
+    Ti = Pu / 4.0;
+    Td = 0.0;  
+    break;
   case ZIEGLER_NICHOLS_PI:
   default:
     Kp = 0.4   * Ku;
-    Ti = 0.45  * Pu;
+    Ti = 0.8   * Pu;
     Td = 0.0;
   }
+
+  // converged
+  return true;
 }
 
 double PID_ATune::GetKp()
@@ -265,11 +434,11 @@ double PID_ATune::GetNoiseBand()
 
 void PID_ATune::SetLookbackSec(int value)
 {
-  if (value < 1) 
+  if (value < 5) 
   {
-    value = 1;
+    value = 5;
   }
-  else if(value < 25)
+  if (value < 25)
   {
     nLookBack = value * 4;
     sampleTime = 250;
--- PID_AutoTune_v0/PID_AutoTune_v0.h
+++ PID_AutoTune_v0/PID_AutoTune_v0.h
@@ -1,6 +1,6 @@
 #ifndef PID_AutoTune_v0
 #define PID_AutoTune_v0
-#define LIBRARY_VERSION	0.0.1
+#define LIBRARY_VERSION 0.0.1
 
 #include "PID_types.h"
 
@@ -15,56 +15,58 @@ class PID_ATune
 
 public:
   //commonly used functions ********************************************************************
-  PID_ATune(double*, double*);        	// * Constructor.  links the Autotune to a given PID
-  bool Runtime();	 	   	// * Similar to the PID Compute function, 
-					//   returns true when done, otherwise returns false
-  void Cancel();		   	// * Stops the AutoTune	
+  PID_ATune(double*, double*);          // * Constructor.  links the Autotune to a given PID
+  bool Runtime();                       // * Similar to the PID Compute function, 
+                                        //   returns true when done, otherwise returns false
+  void Cancel();                        // * Stops the AutoTune 
 
-  void SetOutputStep(double);	  	// * how far above and below the starting value will 
-					//   the output step?	
-  double GetOutputStep();	 	// 
+  void SetOutputStep(double);           // * how far above and below the starting value will 
+                                        //   the output step?   
+  double GetOutputStep();               // 
 
-  void SetControlType(enum Control);	// * Determines if the tuning parameters returned 
-   					//   will be PI (0) or PID (1)
-  int GetControlType();		   	//   Returns controller type (0=PI, 1=PID)
+  void SetControlType(enum Control);    // * Determines tuning algorithm
+  int GetControlType();                 // * Returns tuning algorithm
 
-  void SetLookbackSec(int);		// * how far back are we looking to identify peaks
-  int GetLookbackSec();			//
+  void SetLookbackSec(int);             // * how far back are we looking to identify peaks
+  int GetLookbackSec();                 //
 
-  void SetNoiseBand(double);		// * the autotune will ignore signal chatter smaller 
-					//   than this value
-  double GetNoiseBand();		//   this should be accurately set
+  void SetNoiseBand(double);            // * the autotune will ignore signal chatter smaller 
+                                        //   than this value
+  double GetNoiseBand();                //   this should be accurately set
 
 #ifdef DITHER
-  void SetDither( double );		// * noise added to input to smooth quantization errors
-  double GetDither();			//   set to smallest step value in input range
+  void SetDither( double );             // * noise added to input to smooth quantization errors
+  double GetDither();                   //   set to smallest step value in input range
 #endif
 
-  double GetKp();			// * once autotune is complete, these functions contain the
-  double GetKi();			//   computed tuning parameters.  
-  double GetKd();			//
+  double GetKp();                       // * once autotune is complete, these functions contain the
+  double GetKi();                       //   computed tuning parameters.  
+  double GetKd();                       //
 
 private:
-  void FinishUp();
-  bool isMax, isMin;
-  double *input, *output;
+  bool CheckStable();                   // * check whether recent inputs have similar values
+
+  double *input;
+  double *output;
   double setpoint;
+
+  double oStep;
   double noiseBand;
-  enum Control controlType;
-  bool running;
-  unsigned long peak1, peak2, lastTime;
+  byte nLookBack;
+  enum Control controlType;             // * selects autotune algorithm
+
+  enum AutoTunerState state;            // * state of autotuner finite state machine
+  unsigned long lastTime;
   unsigned long sampleTime;
-  int nLookBack;
+  unsigned long peakTime[3];
   enum Peak peakType;
   double lastInputs[101];
-  double peaks[10];
-  int peakCount;
-  bool justchanged;
-  bool justevaled;
-  double absMax, absMin;
-  double oStep;
+  byte inputCount;
+  double peaks[20];
+  byte peakCount;
   double outputStart;
-  double Ku, Pu, Kp, Ti, Td;
+  double originalNoiseBand;
+  double K_process, Kp, Ti, Td;
 
 #ifdef DITHER
   double dither;
